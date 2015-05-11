e1950887b739e0ad5d6ff46a67e116a0ff1a1012
Mohamed Azzouni <mohamed.azzouni@gmail.com>
2015-05-11 19:24:51 +0200

    setting menu isworking , needs more improvment

--- ang/fjplayer.angular.js
+++ ang/fjplayer.angular.js
@@ -15,6 +15,10 @@ controller('fjplayerCtrl', ['$scope' ,'$filter','$interval','$document' ,'$timeo
     $scope.isFullScreen = false ;
     $scope.isFullScreenSupported = true ;
 
+    $scope.isContainsSubs = false ;
+    $scope.isContainsLangs = false ;
+    $scope.isContainsThumbs = false ;
+
     $scope.VolLevelUp = true ;
     $scope.VolLevelDown = false ;
     $scope.VolLevelOff = false ;
@@ -27,7 +31,8 @@ controller('fjplayerCtrl', ['$scope' ,'$filter','$interval','$document' ,'$timeo
     $scope.isAdsDataHidden = false ;
     $scope.isAdsInfoHidden = false ;
 
-    $scope.thumbTime=0;
+    $scope.thumbTime  = 0;
+    $scope.tracksArray  = {"subs":[],"audio":[]};
     
     $scope.movieCTime = 0;
     $scope.movieTTime  = 0;
@@ -37,10 +42,19 @@ controller('fjplayerCtrl', ['$scope' ,'$filter','$interval','$document' ,'$timeo
     $scope.AdsData ="";
     $scope.AdsInfo ="";
 
-    $scope.ccSubMenu = $sce.trustAsHtml(  '   <p>CC :<ul><li>hola </li><li>hila </li></ul></p>');
-    $scope.langSubMenu = $sce.trustAsHtml( '<p>Lang :<ul><li>hola- </li><li>hila- </li></ul></p>');
-
     $scope.video =  document.getElementById('videoID');
+    $scope.td = document.getElementById('thumbDiv');
+    $scope.t = document.getElementById('thumb');      
+    $scope.b = document.getElementById('hprogressbar');
+/* only for dash
+    var url = "http://bitdash-a.akamaihd.net/content/sintel/sintel.mpd ";
+    //http://dash.edgesuite.net/dash264/TestCases/1c/qualcomm/2/MultiRate.mpd";
+    var context = new Dash.di.DashContext();
+    var player = new MediaPlayer(context);
+    player.startup();
+    player.attachView(document.querySelector("#videoID"));
+    player.attachSource(url);
+*/
 
     $scope.setVolume = function(newVolumePercentage){
       $scope.video.volume =  (newVolumePercentage /100);
@@ -68,17 +82,9 @@ controller('fjplayerCtrl', ['$scope' ,'$filter','$interval','$document' ,'$timeo
           $scope.videoReady = true;          
           $scope.goPlay();
           console.log("Video is loaded and can be played ; READY", $scope.videoReady,"volume ",$scope.volume);
-
-          // Check if fullscreen supported. If it's not just don't show the fullscreen icon.
-          if(!$scope.video.requestFullscreen && 
-             !$scope.video.mozRequestFullScreen &&
-             !$scope.video.webkitRequestFullScreen) {
-                $scope.isFullScreenSupported  = false;
-          }
-
+          $scope.loadModuleMetadata();          
       })       
     }, false);
-
     $scope.video.addEventListener('timeupdate', function () {
       $scope.$apply(function () {          
           $scope.movieTTime  = $scope.video.duration ;
@@ -97,6 +103,7 @@ controller('fjplayerCtrl', ['$scope' ,'$filter','$interval','$document' ,'$timeo
       // TODO  ..      
     });
     $scope.video.addEventListener('error', function () {
+      console.error('error on video !!');
       // TODO  ..      
     });
     $scope.video.addEventListener('waiting', function () {
@@ -151,11 +158,9 @@ controller('fjplayerCtrl', ['$scope' ,'$filter','$interval','$document' ,'$timeo
       }     
     };
 
-
-
     $scope.goSeek = function ($event) {  
         var rect = $scope.b.getBoundingClientRect();
-        var p = ($event.pageX - rect.left ) * (  $scope.v.duration / (rect.right - rect.left) );
+        var p = ($event.pageX - rect.left ) * (  $scope.video.duration / (rect.right - rect.left) );
           
         //pause
         if( $scope.isPlaying === true )
@@ -165,56 +170,83 @@ controller('fjplayerCtrl', ['$scope' ,'$filter','$interval','$document' ,'$timeo
         }
 
         //change current time 
-        $scope.v.currentTime = p;
+        $scope.video.currentTime = p;
 
         //play
         $scope.isPlaying  =  true;
         $scope.video.play(); 
     };
 
-    // init 
-    $scope.indexThumbsTrack = -1 ;
-    $scope.td = document.getElementById('thumbDiv');
-    $scope.t = document.getElementById('thumb');
-    $scope.v = document.getElementById('videoID');
-    $scope.b = document.getElementById('hprogressbar');
-    
-    // looking for metadata tracks
-    for (var i = 0; i < $scope.v.textTracks.length; i++) {
-      if ( $scope.v.textTracks[i].kind == 'metadata' ){       
-        $scope.indexThumbsTrack = i ; 
-        //console.log("metadata track found @ index ",$scope.indexThumbsTrack);        
-        break ;
+    $scope.loadModuleMetadata = function (){
+      // init 
+      $scope.indexThumbsTrack = -1 ;
+      
+      // Check if fullscreen supported. If it's not just don't show the fullscreen icon.
+      if(!$scope.video.requestFullscreen && 
+         !$scope.video.mozRequestFullScreen &&
+         !$scope.video.webkitRequestFullScreen) {
+            $scope.isFullScreenSupported  = false;
+      }
+
+      // looking for metadata  & subtitles tracks
+      if($scope.video.textTracks) {
+        var newObj ={"label" : "off", "index":-1 , "actif":true};
+        $scope.tracksArray.subs.push(newObj);
+        for (var i = 0; i < $scope.video.textTracks.length; i++) {
+          if ( $scope.video.textTracks[i].kind == 'metadata' )
+          {       
+            $scope.indexThumbsTrack = i ; 
+            $scope.isContainsThumbs = true ;
+          }
+          else if($scope.video.textTracks[i].kind == 'subtitles' )
+          {
+            newObj =  {"label" : $scope.video.textTracks[i].label ,"index":i, "actif":false} ;            
+            $scope.tracksArray.subs.push( newObj);            
+            $scope.isContainsSubs = true ;
+          }
+          else
+          {
+            console.log ("unknown type of tracks ",$scope.video.textTracks[i].kind)
+          }
+        } 
+      }
+
+      // looking for audio tracks 
+      if($scope.video.audioTracks) {
+        var newObj ;
+        for (var i = 0; i < $scope.video.audioTracks.length; i++) {
+          $scope.isContainsLangs = true ;
+          newObj = {"label" : $scope.video.audioTracks[i].language ,"index":i , "actif":true}; 
+          $scope.tracksArray.audio.push(  newObj);
+          console.log(" > audio are ",$scope.audioArray);
+        }
       }
     };
 
-
     // Thumbs WebVtt Plugin Function
     $scope.goShowThumbs = function ($event) {
-        if($scope.indexThumbsTrack == -1 )
-          return;
-        $scope.td.style.visibility = 'visible';
+        if($scope.isContainsThumbs )          
+          $scope.td.style.visibility = 'visible';
     };
     $scope.goHideThumbs = function ($event) {
-      if($scope.indexThumbsTrack == -1 )
-        return;
-      $scope.td.style.visibility = 'hidden';
+      if($scope.isContainsThumbs )
+        $scope.td.style.visibility = 'hidden';
     };
     $scope.goRenderThumbs = function ($event) {   
-      if($scope.indexThumbsTrack == -1 )
+      if($scope.indexThumbsTrack == false )
         return;
 
       // first we convert from mouse to time position ..
       var rect = $scope.b.getBoundingClientRect();
-      var p = ($event.pageX - rect.left ) * (  $scope.v.duration / (rect.right - rect.left) );
+      var p = ($event.pageX - rect.left ) * (  $scope.video.duration / (rect.right - rect.left) );
       
-      if ( ( p > ($scope.v.duration + 2)) || (p < 0) )//some error ?
+      if ( ( p > ($scope.video.duration + 2)) || (p < 0) )//some error ?
         return ; 
       //update ui
       $scope.thumbTime = p;
       //console.log(">>>>>>>>> CURRENT :", $scope.thumbTime ,">>>>>>>>> CURRENT :", p);
       // ..then we find the matching cue..
-      var c = $scope.v.textTracks[$scope.indexThumbsTrack].cues;
+      var c = $scope.video.textTracks[$scope.indexThumbsTrack].cues;
       if( c == null) //track eleme,t is not supprted : Firefox 
         return;
     
@@ -285,13 +317,64 @@ controller('fjplayerCtrl', ['$scope' ,'$filter','$interval','$document' ,'$timeo
     };
 
     //subtitles and setting 
-    $scope.goSubtitles  = function () {      
+    $scope.goSubtitles  = function () {            
+      console.info("arrays tracks ", $scope.tracksArray );
       var menudiv = document.getElementById('settingMenuDiv');
+      // second call :to  hide 
+      if(menudiv.style.visibility === 'visible')
+        menudiv.style.visibility = 'hidden';  
+
       var subsBtn = document.getElementById('subsBtnId');
       var rect = subsBtn.getBoundingClientRect();
       menudiv.style.left = rect.left +'px';
-      menudiv.style.top = rect.top  - (rect.height *2.5)+'px'           
+      menudiv.style.top = rect.top  - (rect.height * Math.max($scope.tracksArray.subs.length, $scope.tracksArray.audio.length))+'px'           
       menudiv.style.visibility = 'visible';
+    };
+    $scope.setSubs = function( index) {
+        console.log("Setting Subs to index ", index );
+        //json array 
+        for (var i=0; i< $scope.tracksArray.subs.length; i++) {
+          if($scope.tracksArray.subs[i].index == index ){
+            console.log("actif to true index ", i  );
+            $scope.tracksArray.subs[i].actif = true ;
+          }
+          else {
+            console.log("actif to false index ", i  );
+            $scope.tracksArray.subs[i].actif = false ;
+          }
+        }
+        //video array
+        for (var i=0; i< $scope.video.textTracks.length; i++) {
+          if (i == index) {
+            $scope.video.textTracks[i].mode = 'showing';
+          } else {
+            $scope.video.textTracks[i].mode = 'hidden';
+          }
+        }
+        //finish
+        var menudiv = document.getElementById('settingMenuDiv');
+        menudiv.style.visibility = 'hidden';
+    };
+    $scope.setAudio = function( index) {
+      console.log("Setting Audio to index ", index );
+      //json array 
+      for (var i=0; i< $scope.tracksArray.audio.length; i++) {
+        if($scope.tracksArray.audio[i].index == index )
+          $scope.tracksArray.audio[i].actif = true ;
+        else 
+          $scope.tracksArray.audio[i].actif = false ;
+      }
+      //video array
+      for (var i=0; i< $scope.video.videoTracks.length; i++) {
+        if (i == index) {
+          $scope.video.videoTracks[i].selected = true;
+        } else {
+          $scope.video.videoTracks[i].selected = false;
+        }
+      }
+      //finish
+      var menudiv = document.getElementById('settingMenuDiv');
+      menudiv.style.visibility = 'hidden';
     };
 }]).
 filter('duration', function() {
@@ -326,3 +409,4 @@ run(function() { // instance-injector
   // You can only inject instances (not Providers)
   // into run blocks
 });
+
