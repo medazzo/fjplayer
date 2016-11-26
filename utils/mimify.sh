bfcbd175de3dfe5118c18be5c8dc7cc8459ccd41
mazzouni <mohamed.azzouni@gmail.com>
2016-11-27 00:43:33 +0100

    mimifications

--- utils/mimify.sh
+++ utils/mimify.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 
-#fj.js
-../node_modules/uglify-js/bin/uglifyjs  ../dist/fjplayer.js --comments -o ../dist/fjplayer.min.js
-#fj.css
+
+#fjplayer.min.js
+../node_modules/uglify-js/bin/uglifyjs    ./base64.js ../dist/fjplayer.js --preamble  "/* Fjplayer is a copyright @ EASYSOFTIN */" -o ../dist/fjplayer.min.js --stats
+#fjplayer.all.min.js
+../node_modules/uglify-js/bin/uglifyjs  ../bower_components/dashjs/dist/dash.min.js  ../bower_components/jquery/dist/jquery.min.js ../bower_components/angular/angular.min.js \
+    ../bower_components/bootstrap/dist/js/bootstrap.min.js ../dist/fjplayer.min.js \
+         --preamble "/* Fjplayer is a copyright @ EASYSOFTIN */"  -o ../dist/fjplayer.all.min.js --stats
+#fjplayer.min.css
 ../node_modules/uglifycss/uglifycss --max-line-len 0 --ugly-comments ../dist/fjplayer.css   > ../dist/fjplayer.min.css
-#dash.js
-#../node_modules/uglify-js/bin/uglifyjs  ./fjdash.debug.js --charset utf-8 --nomunge --type js --preserve-semi -o ./fjdash.min.js
-#b64.js
-#../node_modules/uglify-js/bin/uglifyjs  ./fjbase64.js --charset utf-8 --nomunge --type js --preserve-semi -o ./fjbase64.min.js
+
