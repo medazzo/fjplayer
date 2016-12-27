f1a91558c5e8df1a0d2e088d0e234ca5963a282f
mazzouni <mohamed.azzouni@gmail.com>
2016-12-27 03:31:35 +0100

    start to do thumbs

--- src/html/commands.js
+++ src/html/commands.js
@@ -6,21 +6,23 @@ var volBtns = new Array(10);
 var widthV = new Array(10);
 var savVolume = new Array(10);
 var barVolume = new Array(10);
-var progVolume = new Array(10);
+var barProgress = new Array(10);
 var durations = new Array(10);
 var muted = new Array(10);
 var expanded = new Array(10);
 var fullScreen = new Array(10);
+var indexThumbsTrack = new Array(10);
+var tdObj = new Array(10);
+var tiObj = new Array(10);
 var refreshed;
 var maxUsedVid = 0;
 
 function timerChecks(index) {
     for (var index = 0; index < maxUsedVid; index++) {
-        var p = Math.round(videos[index].duration / 100);
+        var p = Math.round(videos[index].duration / 1000);
         var c = Math.round(videos[index].currentTime / p);
-        progVolume[index].value = c;
+        barProgress[index].value = c;
         durations[index].innerHTML = ' <span>' + duration(videos[index].currentTime) + '</span><span>/</span><span>' + duration(videos[index].duration) + '</span>';
-        console.log('>>>> Ping @ ', index);
     }
 }
 
@@ -32,20 +34,37 @@ function initUI(max) {
         fullScreen[index] = false;
         savVolume[index] = 100;
         videos[index] = document.getElementById('Video' + index);
+        console.log('>>>> Ping @ ', index);
         playBtns[index] = document.getElementById('playBtn' + index);
+        console.log('>>>> Ping @ ', index);
         volBtns[index] = document.getElementById('volume' + index);
+        console.log('>>>> Ping @ ', index);
         barVolume[index] = document.getElementById('bvolume' + index);
-        progVolume[index] = document.getElementById('bprogress' + index);
+        console.log('>>>> Ping @ ', index);
+        barProgress[index] = document.getElementById('bprogress' + index);
+        console.log('>>>> Ping @ ', index);
         durations[index] = document.getElementById('timer' + index);
+        console.log('>>>> Ping @ ', index);
+        tdObj[index] = document.getElementById('thumbDiv' + index);
+        console.log('>>>> Ping @ ', index);
+        tiObj[index] = document.getElementById('thumbImgBlock' + index);
+        console.log('>>>> Ping @ ', index);
         barVolume[index].value = savVolume[index];
-        progVolume[index].value = 0;
+        barProgress[index].value = 0;
         if (!videos[index].paused) {
             playBtns[index].innerHTML = '<span class=\"fa fa-pause\" title=\"pause\" onclick=\"goPlay(' + index + ')\">';
             console.log('pausing ');
         }
         widthV[index] = videos[index].videoWidth;
+        for (var i = 0; i < videos[index].textTracks.length; i++) {
+            if (videos[index].textTracks[i].kind === 'metadata') {
+                indexThumbsTrack[index] = i;
+                console.log('metadata @ ', i);
+            }
+        }
         console.log('>>>> LOADED @ ', index);
     }
+
     refreshed = setInterval(function() {
         timerChecks();
     }, 1000);
@@ -148,10 +167,6 @@ function seek(index, val) {
     }
 }
 
-function showPic(index, val) {
-
-}
-
 function duration(secDuration) {
     var sec_num = parseInt(secDuration, 10); // don't forget the second param  
     var hours = Math.floor(sec_num / 3600);
@@ -167,4 +182,49 @@ function duration(secDuration) {
         if (hours < 10) { hours = "0" + hours; }
         return (hours + ':' + minutes + ':' + seconds);
     }
+}
+
+function goShowThumbs(index) {
+    tdObj[index].style.visibility = 'visible';
+}
+
+function goHideThumbs(index) {
+    tdObj[index].style.visibility = 'hidden';
+}
+
+function goRenderThumbs(index, event) {
+    // first we convert from mouse to time position ..
+    var rect = barProgress[index].getBoundingClientRect();
+    var p = (event.pageX - rect.left) * (videos[index].duration / (rect.right - rect.left));
+    if ((p > (videos[index].duration + 2)) || (p < 0)) { //some error ?
+        console.error(" Position is bigger than duration >>", p, videos[index].duration);
+        return;
+    }
+    //update ui
+    var thumbTime = p;
+    // ..then we find the matching cue..
+    var c = videos[index].textTracks[indexThumbsTrack[index]].cues;
+    if (c == null) { //track eleme,t is not supprted : Firefox 
+        console.error(" cues is null @ ", indexThumbsTrack[index], " not supported , Firefox ?");
+        return;
+    }
+
+    for (var i = 0; i < c.length; i++) {
+        if (c[i].startTime <= p && c[i].endTime > p) {
+            break;
+        };
+    }
+    // ..next we unravel the JPG url and fragment query..
+    var url = c[i].text.split('#')[0];
+    var xywh = c[i].text.substr(c[i].text.indexOf("=") + 1).split(',');
+
+    // ..and last we style the thumbnail overlay
+    tiObj[index].style.backgroundImage = 'url(' + c[i].text.split('#')[0] + ')';
+    tiObj[index].style.backgroundPosition = '-' + xywh[0] + 'px -' + xywh[1] + 'px';
+    tiObj[index].style.width = xywh[2] + 'px';
+    tiObj[index].style.height = xywh[3] + 'px';
+
+    tdObj[index].style.left = event.pageX - xywh[2] / 2 + 'px';
+    tdObj[index].style.top = rect.top - (xywh[3] * 1.5) + 'px'
+    tdObj[index].style.width = xywh[2] + 'px';
 }
