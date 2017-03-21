eb7def8a42419608dc8bb33c1621a326d69f341f
mazzouni <mohamed.azzouni@gmail.com>
2017-03-22 00:26:18 +0100

    separate code for aud and subs mgt

---
+++ src/AudsMenu.js
@@ -0,0 +1,109 @@
+import Logger from './Logger';
+require('./player.css');
+/**
+ *  Class player in whinch the player is implemented
+ */
+function AudsMenu(video, AudsbuttonID, audMenuContainerDivId, menuListAud) {
+    this.logger = new Logger(this);
+    this.video = video;
+    this.AudsbuttonID = AudsbuttonID;
+    this.audMenuContainerDivId = audMenuContainerDivId;
+    this.menuListAud = menuListAud;
+    this.Audsbutton = document.getElementById(this.AudsbuttonID);
+    this.SubsExist = false;
+    this.Setup();
+};
+/**
+ * Aubs cbx Mgt
+ */
+AudsMenu.prototype.activateAudio = function(self, item) {
+    var i = 0;
+    var litem;
+    var index = Array.prototype.indexOf.call(self.menuListAud.childNodes, item);
+    var tindex = item.getAttribute('index');
+    self.logger.log('clicked is  selected @ index ', index, ' text index ', tindex);
+    if (self.video.audioTracks) {
+        if (self.video.audioTracks[index].enabled) {
+            self.logger.log('AlREADY  selected @ index ', tindex);
+            return;
+        }
+
+        for (i = 0; i < self.menuListAud.children.length; i++) {
+            litem = self.menuListAud.children[i];
+            self.logger.log('cheking item @  ', i);
+            if (i === index) {
+                self.video.audioTracks[i].enabled = true;
+                litem.className = 'subtitles-menu-item-actif';
+                self.logger.log('Setting item @  ', i);
+            } else {
+                self.video.audioTracks[i].enabled = false;
+                litem.className = 'subtitles-menu-item';
+                self.logger.log('Unsetting item @  ', i);
+            }
+        }
+    }
+    self.audMenuDiv.style.display = 'none';
+};
+/**
+ * Event CALLBACK ; called on menu Click
+ */
+AudsMenu.prototype.onshowHideMenu = function(self, menuContainer, ev) {
+    menuContainer.style.position = 'absolute';
+    menuContainer.style.left = (ev.pageX - 20) + 'px';
+    menuContainer.style.top = (ev.pageY - 90) + 'px';
+
+    if (menuContainer.style.display === 'none') {
+        menuContainer.style.display = 'block';
+    } else {
+        menuContainer.style.display = 'none';
+    }
+    self.logger.log(' Showing or Hiding an menu ', menuContainer);
+};
+/**
+ * Setting Subs menu and cbx
+ */
+AudsMenu.prototype.Setup = function() {
+    var i = 0;
+    var item = null;
+    var btn = null;
+    var self = this;
+    var container = document.getElementById(self.audMenuContainerDivId);
+    container.style.display = 'none';
+    if ((!self.video.audioTracks) || (self.video.audioTracks.length <= 1)) {
+        // hide audio button
+        btn = document.getElementById(self.languagesDivId);
+        btn.style.display = 'none';
+        self.logger.log(' Audio Menu not created !');
+        return;
+    }
+    // Add events for languages button
+    self.Audsbutton.addEventListener('click', function(ev) {
+        self.onshowHideMenu(self, self.audMenuDiv, ev);
+    });
+
+    container.className = 'settingMenuDiv';
+    container.innerHTML =
+        '<div class=\"settingMenuSubMenuLeft\" >' +
+        '<ul class=\"subtitles-menu\" id=\"' + self.audMenuListId + '\" >' +
+        '</ul>	' +
+        '</div>';
+
+    // video array
+    self.menuListAud = document.getElementById(self.audMenuListId);
+    if (self.video.audioTracks) {
+        for (i = 0; i < self.video.audioTracks.length; i++) {
+            item = document.createElement('li');
+            if (self.video.audioTracks[i].enabled) {
+                item.className = 'subtitles-menu-item-actif';
+            } else {
+                item.className = 'subtitles-menu-item';
+            }
+            item.innerHTML = self.video.audioTracks[i].language + '::' + self.video.audioTracks[i].label;
+            self.menuListAud.appendChild(item);
+            item.addEventListener('click', function(ev) {
+                self.activateAudio(self, this);
+            });
+        }
+    }
+    self.logger.log(' Audio Menu created !', self.video.audioTracks.length, '! ', self.menuListAud);
+};
--- src/SubsMenu.js
+++ src/SubsMenu.js
@@ -1,18 +1,17 @@
 import Logger from './Logger';
-import Player from './playlist';
-import * as Const from './constants';
 require('./player.css');
-require('font-awesome/css/font-awesome.css');
 /**
  *  Class player in whinch the player is implemented
  */
-function SubsMenu(video, player, subsExtraList) {
+function SubsMenu(video, SubsbuttonID, subsdMenuContainerDivId, subsMenuListId) {
     this.logger = new Logger(this);
-    this.fjplayer = player;
-    this.fjvideo = video;
-    this.fjsubsExtraLists = subsExtraList;
-    // prepare player
-
+    this.video = video;
+    this.SubsbuttonID = SubsbuttonID;
+    this.subsdMenuContainerDivId = subsdMenuContainerDivId;
+    this.subsMenuListId = subsMenuListId;
+    this.SubsExist = false;
+    this.subtitlesBtn = document.getElementById(this.SubsbuttonID);
+    this.Setup();
 };
 /**
  * cbx Subs Mgt
@@ -50,7 +49,7 @@ SubsMenu.prototype.activateSubs = function(self, item) {
 /**
  * Event CALLBACK ; called on menu Click
  */
-Player.prototype.onshowHideMenu = function(self, menuContainer, ev) {
+SubsMenu.prototype.onshowHideMenu = function(self, menuContainer, ev) {
     menuContainer.style.position = 'absolute';
     menuContainer.style.left = (ev.pageX - 20) + 'px';
     menuContainer.style.top = (ev.pageY - 90) + 'px';
@@ -65,50 +64,47 @@ Player.prototype.onshowHideMenu = function(self, menuContainer, ev) {
 /**
  * Setting Subs menu and cbx
  */
-SubsMenu.prototype.Setup = function(SubsbuttonID, subsdMenuContainerDivId, subsMenuListId) {
+SubsMenu.prototype.Setup = function() {
     var i = 0;
     var item = null;
-    var foundsubs = false;
-    var container = document.getElementById(subsdMenuContainerDivId);
-
+    var container = document.getElementById(this.subsdMenuContainerDivId);
+    var self = this;
     // video array
-    self.menuListSubs = document.getElementById(subsMenuListId);
-    for (i = 0; i < self.video.textTracks.length; i++) {
-        if ((self.video.textTracks[i].kind === 'captions') ||
-            (self.video.textTracks[i].kind === 'subtitles')) {
-            foundsubs = true;
+    this.menuListSubs = document.getElementById(this.subsMenuListId);
+    for (i = 0; i < this.video.textTracks.length; i++) {
+        if ((this.video.textTracks[i].kind === 'captions') ||
+            (this.video.textTracks[i].kind === 'subtitles')) {
+            this.SubsExist = true;
             item = document.createElement('li');
-            if (self.video.textTracks[i].mode === 'showing') {
+            if (this.video.textTracks[i].mode === 'showing') {
                 item.className = 'subtitles-menu-item-actif';
             } else {
                 item.className = 'subtitles-menu-item';
             }
             item.setAttribute('index', i);
-            item.innerHTML = self.video.textTracks[i].label;
-            self.menuListSubs.appendChild(item);
+            item.innerHTML = this.video.textTracks[i].label;
+            this.menuListSubs.appendChild(item);
             item.addEventListener('click', function(ev) {
                 self.activateSubs(self, this);
             });
-            self.logger.log('Setting Subs List @ ', i, ' item is ', item);
+            this.logger.log('Setting Subs List @ ', i, ' item is ', item);
         }
     }
-    if (foundsubs === true) {
+    if (this.SubsExist === true) {
         // Add events for subtitles button
-        self.subtitlesBtn.addEventListener('click', function(ev) {
+        this.subtitlesBtn.addEventListener('click', function(ev) {
             self.onshowHideMenu(self, self.subsMenuDiv, ev);
         });
 
         container.className = 'settingMenuDiv';
         container.innerHTML =
             '<div class=\"settingMenuSubMenuLeft\" >' +
-            '<ul class=\"subtitles-menu\" id=\"' + self.subsMenuListId + '\" >' +
+            '<ul class=\"subtitles-menu\" id=\"' + this.subsMenuListId + '\" >' +
             '</ul>	' +
             '</div>';
         container.style.display = 'none';
-        self.logger.log(' Subs Menu  created !! ', self.menuListSubs);
+        this.logger.log(' Subs Menu  created !! ', this.menuListSubs);
     } else {
-        self.logger.log(' Subs Menu Not created !! ');
+        this.logger.log(' Subs Menu Not created !! ');
     }
 };
-// constantes member of class
-SubsMenu.prototype.playlistLoaded = false;
