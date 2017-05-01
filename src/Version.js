68ac69a2dc1dcc68c43b73cf37d172f1f60fa3f8
mazzouni <mohamed.azzouni@gmail.com>
2017-05-01 02:51:19 +0200

    recompile and update version after tag

--- src/Version.js
+++ src/Version.js
@@ -3,9 +3,9 @@
 function Version() {
 };
 
-const GIT_VERSION = '0.2.7';
-const GIT_COMMIT_SINCE_TAG = '5';
-const GIT_HEAD_SHORT_HASH = 'gc592fde';
+const GIT_VERSION = '0.2.8';
+const GIT_COMMIT_SINCE_TAG = '';
+const GIT_HEAD_SHORT_HASH = '';
 
 Version.prototype.getVersion = function() {
     return GIT_VERSION;
