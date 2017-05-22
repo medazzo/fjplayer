4906dd111aea225fa503c9c49dce679f95959b11
mazzouni <mohamed.azzouni@gmail.com>
2017-05-22 23:12:47 +0200

    updating dist folder

---
+++ src/Version.js
@@ -0,0 +1,22 @@
+/* Version.js File */
+
+function Version() {
+};
+
+const GIT_VERSION = '0.2.10';
+const GIT_COMMIT_SINCE_TAG = '';
+const GIT_HEAD_SHORT_HASH = '';
+
+Version.prototype.getVersion = function() {
+    return GIT_VERSION;
+};
+
+Version.prototype.getCommits = function() {
+    return GIT_COMMIT_SINCE_TAG;
+};
+
+Version.prototype.getShortHash = function() {
+    return GIT_HEAD_SHORT_HASH;
+};
+
+export default Version;
