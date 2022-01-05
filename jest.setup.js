c7722cbc554eba936094cd894384b3e481055089
Mohamed Azzouni <mohamed.azzouni@gmail.com>
2022-01-05 22:51:43 +0100

    some config updates

--- jest.setup.js
+++ jest.setup.js
@@ -1,6 +1,5 @@
 global.console = {
   log: jest.fn(), // console.log are ignored in tests
-
   // Keep native behaviour for other methods, use those to print out things in your own tests, not `console.log`
   error: jest.fn(), // console.error are ignored in tests
   warn: console.warn,
